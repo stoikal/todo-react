b36aa89d8834bf5c7714baa0d0daf615b2d22648
Laksito Pamilih <laksito@protonmail.com>
2023-06-22 14:55:53 +0700

    add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${pkg.name}/`,
   plugins: [
     UnoCSS(),
-    react(),
-  ],
+    react()
+  ]
 })
